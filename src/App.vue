d05aeb94eda057a9285df0ee93f32db3931cbdc8
Adam Christopher <adam.keristoper@gmail.com>
2018-07-22 02:19:33 +0900

    Create base vue app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,14 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <top-page/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TopPage from './components/TopPage/index.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    TopPage
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/TopPage/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <p>this is the top page</p>
+  </div>
+</template>
