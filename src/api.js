94b7701f765272bc8905a1a37b6e874d527d598e
Adam Christopher <adam.keristoper@gmail.com>
2018-07-28 10:30:56 +0900

    Marker works

--- src/api.js
+++ src/api.js
@@ -3,8 +3,24 @@ import { db } from './constants'
 export function fetchBars () {
   return db.collection('bars').get()
     .then(querySnapshot => {
+      let resultArray = []
       querySnapshot.forEach(doc => {
-        console.log(`id ${doc.id} => ${doc.data()}`)
+        resultArray.push(doc.data())
       })
+      return resultArray
     })
+}
+
+/**
+* Call this function after gmaps are loaded
+* @param {GoogleMapObject}
+* @param {Number}
+* @param {Number}
+* @return {GoogleMapMarkerObject}
+*/
+export function createMarker (map, lat, lng) {
+  return new google.maps.Marker({
+    map,
+    position: { lat, lng }
+  })
 }
--- src/components/TopPage/index.vue
+++ src/components/TopPage/index.vue
@@ -1,16 +1,51 @@
 <template>
   <GmapMap
+    ref="gmap"
     :center="{lat:35.658725, lng:139.702118}"
     :zoom="13"
-  />
+  >
+    <GmapMarker
+      :key="index"
+      v-for="(m, index) in markers"
+      :position="m.position"
+      :clickable="true"
+      @click="center=m.position"
+    />
+  </GmapMap>
 </template>
 
 <script>
-import { fetchBars } from '../../api'
+/* eslint-disable */
+import { fetchBars, createMarker } from '../../api'
 
 export default {
-  created () {
-    fetchBars()
+  data () {
+    return {
+      map: null,
+      geocoder: null,
+      markers: []
+    }
+  },
+  mounted () {
+    // load map & geocoder first, then fetch bars
+    this.$refs.gmap.$mapPromise.then(map => {
+      this.map = map,
+      this.geocoder = new google.maps.Geocoder()
+      return fetchBars()
+    }).then(rawBars => {
+      rawBars.map(bar => {
+        if (bar.lat && bar.lng) {
+          const oneMark = createMarker(this.map, bar.lat, bar.lng)
+          this.markers.push(oneMark)
+        }
+      })
+
+      // geocoder.geocode({ 'address': '中野区本町2-51-9' }, (results, status) => {
+      //   console.log(results)
+      //   console.log(status)
+      //   console.log(results[0].geometry.location.lat())
+      // })
+    }).catch(error => console.error('Maps or fetching bars failing, maybe it\'s the frikkin internet', error))
   }
 }
 </script>
