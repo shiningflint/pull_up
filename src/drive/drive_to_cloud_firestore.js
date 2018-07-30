a2807724f46b8a30deb0f755615015210e87a8aa
Adam Christopher <adam.keristoper@gmail.com>
2018-07-30 22:59:52 +0900

    Adding JS script drive to firebase

--- src/drive/drive_to_cloud_firestore.js
+++ src/drive/drive_to_cloud_firestore.js
@@ -1,1 +1,13 @@
+const GoogleSpreadsheet = require('google-spreadsheet')
+const googleSpreadsheetKey = require('../../keys.js').googleSpreadsheetKey
+
+const doc = new GoogleSpreadsheet(googleSpreadsheetKey)
+
+console.log(doc)
+
+doc.getInfo(function(err, info) {
+  console.log('got err', err)
+  console.log('got some info', info)
+})
+
 console.log('download bananas from drive and insert to potatoes')
