67d62a0f7e0a182f3a643f42ea8a09fdef38b304
Adam Christopher <adam.keristoper@gmail.com>
2018-08-08 14:18:55 +0900

    Added firebase to the mix

--- src/drive/drive_to_cloud_firestore.js
+++ src/drive/drive_to_cloud_firestore.js
@@ -1,6 +1,21 @@
 const GoogleSpreadsheet = require('google-spreadsheet')
 const GoogleSpreadsheetKey = require('../../keys.js').googleSpreadsheetKey
 
+// firebase stuff
+const firebase = require('firebase')
+const firebaseConfig = {
+  apiKey: "AIzaSyAIzFKrSOR5SRneMbSmNz4gSdRiOk8KNYI",
+  authDomain: "pull-up-dd6a6.firebaseapp.com",
+  databaseURL: "https://pull-up-dd6a6.firebaseio.com",
+  projectId: "pull-up-dd6a6",
+  storageBucket: "pull-up-dd6a6.appspot.com",
+  messagingSenderId: "447491243109"
+}
+firebase.initializeApp(firebaseConfig)
+const db = firebase.firestore()
+const settings = {/* your settings... */ timestampsInSnapshots: true}
+db.settings(settings)
+
 const doc = new GoogleSpreadsheet(GoogleSpreadsheetKey)
 
 const WorkSheets = {
@@ -12,22 +27,25 @@ const WorkSheets = {
 
 const selectedSheet = WorkSheets.chuoku
 
-function gotSheets (err, info) {
-  // console.log('got err', err)
-  // console.log('got some info', info)
-  info.worksheets.map(worksheet => {
-    if (worksheet.title === selectedSheet.worksheetTitle) {
-      console.log('save to firebase', worksheet)
-    }
+function getSheetInfo (doc, worksheetTitle) {
+  return new Promise(resolve => {
+    doc.getInfo((err, info) => {
+      info.worksheets.map(worksheet => {
+        if (worksheet.title === worksheetTitle) resolve(worksheet)
+      })
+    })
   })
 }
 
-function getSheetInfo (doc) {
-  doc.getInfo(gotSheets)
-}
-
 function init () {
-  getSheetInfo(doc)
+  getSheetInfo(doc, selectedSheet.worksheetTitle).then(worksheet => {
+    console.log('bananas', worksheet)
+  })
+  // db.collection('bars').add({
+  //   bananas: 'pisang',
+  //   potatoes: 'kentang'
+  // })
+  return
 }
 
 init()
